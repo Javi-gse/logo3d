d095547ac23a48204c3ee9845df8f342ba9faa6f
Javi-gse <72070422+Javi-gse@users.noreply.github.com>
2021-03-16 00:03:47 +0100

    Add files via upload

---
+++ logo-3d/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'adamant'
+game 'gta5'
+
+author 'Javiツ#6046'
+description 'Simple html file logo 3d in screen'
+
+ui_page ('html/index.html')
+
+files ({
+  'html/index.html',
+  'html/logo.png'
+})
